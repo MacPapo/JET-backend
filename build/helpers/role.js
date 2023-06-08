88d36998e272a1e912b8981efa7500f945d2ccaf
MacPapo <jacopocostantini32@gmail.com>
2023-06-08 10:15:19 +0200

    Refactoring, token and more feature added

--- build/helpers/role.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (...roleCodes) => (req, res, next) => {
-    req.currentRoleCodes = roleCodes;
-    next();
-};
-//# sourceMappingURL=role.js.map
